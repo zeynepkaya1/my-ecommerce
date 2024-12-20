42c846c96e0f95d31455451c3b5c2fe4fdeeebc3
Zeynep <1998.ze.ka@gmail.com>
2024-12-20 16:37:57 +0100

    github page correction and routing

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,9 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import { createRouter, createWebHistory } from "vue-router";
+import App from "./App.vue";
+import router from "./router";
 
 import "./style.css";
-import App from "./App.vue";
-import ProductList from "./components/ProductList.vue";
-import ProductDetail from "./components/ProductDetail.vue";
-import CartPage from "./components/CartPage.vue";
-import CheckoutPage from "./components/CheckoutPage.vue";
-
-// Define the routes
-const routes = [
-  { path: "/", component: ProductList, name: "Home" },
-  { path: "/product/:id", component: ProductDetail, name: "ProductDetail" },
-  { path: "/cart", component: CartPage, name: "Cart" },
-  { path: "/checkout", component: CheckoutPage, name: "Checkout" },
-];
-
-// Create the router instance
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
 
 const app = createApp(App);
 app.use(createPinia());
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "404.html",
+          dest: "",
+        },
+      ],
+    }),
+  ],
+  base: "/my-ecommerce/",
+});
